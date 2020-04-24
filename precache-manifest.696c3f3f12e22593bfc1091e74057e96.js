b6048dc856425723175f40afa36e0f43e9998a36
Nahuel <nahuee.10.96@gmail.com>
2020-04-23 22:00:49 -0300

    Updates

---
+++ precache-manifest.696c3f3f12e22593bfc1091e74057e96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23714091da5951bcbcd54a86e31c7af",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "66abcd0f103a65ad3322",
+    "url": "/repository-name/static/css/main.a53130f4.chunk.css"
+  },
+  {
+    "revision": "6dbb7dea96f16f30288e",
+    "url": "/repository-name/static/js/2.1ab0dbde.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/repository-name/static/js/2.1ab0dbde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66abcd0f103a65ad3322",
+    "url": "/repository-name/static/js/main.b10bda4c.chunk.js"
+  },
+  {
+    "revision": "79b98787a4faadd12a6e",
+    "url": "/repository-name/static/js/runtime-main.5a4a9892.js"
+  },
+  {
+    "revision": "400dc88500c0bc5e11f6c953dedcc7ab",
+    "url": "/repository-name/static/media/twitter.400dc885.png"
+  }
+]);
